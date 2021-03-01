f42029cf3a202c6b0173039c5c088656f03fa1da
leehep <leehelo@gmail.com>
2021-03-01 12:36:09 +0200

    first uplode

---
+++ src/components/DisplayRandomCocktail.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '100%', 
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  
+  // avatar: {
+  //   backgroundColor: red[500],
+  // },
+}));
+
+export default function DisplayRandomCocktail({coctailData}) {
+  const classes = useStyles();
+ 
+  const cardHeader = (
+    <CardHeader
+      title={coctailData.strDrink}
+      subheader={`${coctailData.strAlcoholic} ${coctailData.strCategory}`}
+    />
+  )
+
+  const cardMedia = (
+    <CardMedia
+      className={classes.media}
+      image="https://www.thecocktaildb.com/images/media/drink/kztu161504883192.jpg"
+      title="Grim Reaper"
+    />
+  )
+
+  const cardActions = (
+    <CardActions disableSpacing>
+      <IconButton aria-label="add to favorites">
+        <FavoriteIcon />
+      </IconButton>
+    </CardActions>
+  )
+
+  const ingredientList = (
+    <List dense={true}>
+      <ListItem>
+        <ListItemText
+          primary="1 oz - Kahlua"
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="1 oz Bacardi - 151 proof rum"
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="1 dash - Grenadine"
+        />
+      </ListItem>
+    </List>
+  )
+
+  const collapseText = (
+    <CardContent>
+      <Typography>Ingredient:</Typography>
+      {ingredientList}
+      <Typography paragraph>Method:</Typography>
+      <Typography paragraph>
+        "Mix Kahlua and 151 in glass. Quickly add ice and pour grenadine over ice to give ice red tint.",
+      </Typography>
+    </CardContent>
+  )
+
+  console.log(coctailData)
+  return (
+    <Card className={classes.root}>
+      {cardHeader}
+      {cardMedia}
+      {cardActions}
+      {collapseText}
+    </Card>
+  );
+}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,64 @@
+import React,{useEffect} from 'react';
+import TopNevBar from '../components/TopNevBar';
+import DisplayRandomCocktail from '../components/DisplayRandomCocktail'
+import axios from 'axios';
+const _ = require('lodash');
+
+
+// const style = {
+//   p:{
+//     display: "flex",
+//     flexDirection: "column",
+//     flexWrap: "nowrap",
+//     justifyContent: "space-around",
+//     alignItems: "center"
+//   }
+// }
+
+function MainPage(){
+  const [randomCocatail, setRandomCocatail] = React.useState(null);
+  useEffect(()=>{
+    
+    axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then((res)=>{
+        const tempRandomCoctail=_.pickBy(res.data.drinks[0], (ingredient)=>{
+          return ingredient !== null
+        });
+        setRandomCocatail(tempRandomCoctail)
+      })
+  },[])
+
+  return(
+    <div>
+      <TopNevBar/>
+      <div 
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          justifyContent: "space-around",
+          alignItems: "center",
+          // alignContent: 'c enter',
+          width: '70%',
+          margin: 'auto'
+        }}
+      >
+        <p>
+          {/* add css */}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        {randomCocatail!==null?
+          <DisplayRandomCocktail coctailData = {randomCocatail}/>:
+          "no display loading"
+        }
+      </div>
+    </div>
+  )
+}
+
+
+export default MainPage;
